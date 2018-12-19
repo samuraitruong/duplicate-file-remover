2947f453ff198f318d6294f7d92aaa29c106ae51
Truong Nguyen <samuraitruong@hotmail.com>
2018-12-19 21:02:33 +1100

    Add project files.

---
+++ DuplicateFileCleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuplicateFileCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
